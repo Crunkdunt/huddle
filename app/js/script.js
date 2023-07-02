7ed76f369409ce54baf36dd2e26f468260aed7fe
Crunkdunt <48190304+Crunkdunt@users.noreply.github.com>
2023-07-02 23:39:17 +0100

    completed mobile layout
    complete 1st version of mobile layout

---
+++ app/js/script.js
@@ -0,0 +1,6 @@
+//     window.addEventListener('load', function(){
+//         deSVG('.footer_logo', true);
+//     });
+// //Script to turn the logo into white - deSVG https://github.com/benhowdle89/deSVG
+
+// console.log("hi");
